62478f0f399a928585d609a68b6106f7c2a76d6a
ajtmccarty <amccarty@cloudbolt.io>
2020-05-25 12:06:14 -0700

    Remove duplicated model logic from ElevatorProcess class and let the Elevator class handle it

--- src/elevators/instance_ws.py
+++ src/elevators/instance_ws.py
@@ -2,29 +2,30 @@ import asyncio
 import json
 import websockets
 
+from src.common.models import Elevator
 from src.common.utils import get_open_port, get_settings, get_ws_uri
 
 
 __all__ = ["run_elevator"]
 
 
-class Elevator:
+class ElevatorProcess:
     """Elevator class to handle tracking instance variables and the websocket connection"""
 
-    def __init__(self, ws: websockets.WebSocketClientProtocol, host: str, port: int, min_status_wait: float):
+    def __init__(
+        self,
+        ws: websockets.WebSocketClientProtocol,
+        host: str,
+        port: int,
+        min_status_wait: float,
+    ):
         self.ws = ws  # websocket connection
-        self.host = host
-        self.port = port
-        self.floor = None
         self.min_status_wait: float = min_status_wait  # number of seconds to wait between sending statuses
-
-    def as_dict(self) -> dict:
-        """Serialize the instance as a dictionary"""
-        return dict(host=self.host, port=self.port, floor=self.floor)
+        self._elevator: Elevator = Elevator(host=host, port=port)
 
     async def send_status(self):
         """Send status message roughly every status_min_wait settings"""
-        msg_dict: dict = self.as_dict()
+        msg_dict: dict = self._elevator.as_dict()
         msg_bytes = json.dumps(msg_dict).encode("utf-8")
         print(f"Sending status {msg_dict!r}")
         await self.ws.send(msg_bytes)
@@ -32,7 +33,9 @@ class Elevator:
     async def receive_instruction(self) -> bool:
         """Wait to receive an instruction"""
         try:
-            raw_data: bytes = await asyncio.wait_for(self.ws.recv(), timeout=self.min_status_wait)
+            raw_data: bytes = await asyncio.wait_for(
+                self.ws.recv(), timeout=self.min_status_wait
+            )
             # TODO: handle instruction
             return True
         except asyncio.TimeoutError:
@@ -60,10 +63,15 @@ class Elevator:
         # make the connection
         ws = await websockets.connect(ws_uri)
         # make the instance
-        ev = cls(ws=ws, host=config.ELEVATOR_HOST, port=get_open_port(), min_status_wait=config.MIN_STATUS_WAIT)
+        ev = cls(
+            ws=ws,
+            host=config.ELEVATOR_HOST,
+            port=get_open_port(),
+            min_status_wait=config.MIN_STATUS_WAIT,
+        )
         # run the instance
         await ev.run()
 
 
 def run_elevator():
-    asyncio.run(Elevator.create_and_run())
+    asyncio.run(ElevatorProcess.create_and_run())
